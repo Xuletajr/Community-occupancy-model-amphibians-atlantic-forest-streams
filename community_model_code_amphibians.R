b98e3b3bfbc06ea21379a0819d470957ca1507a1
Xuletajr <jwrj@rc.unesp.br>
2018-03-19 15:24:45 -0300

    Read amphibian occurrence data

--- community_model_code_amphibians.R
+++ community_model_code_amphibians.R
@@ -6,3 +6,7 @@
 rm(list=ls(all=TRUE)) # clear memory 
 choose.dir(default = "", caption = "Select folder") # choose a folder
 getwd() # Check the filepath for the working directory
+
+# Read amphibian occurence data of Brazil's Atlantic Forest
+data <- read.csv("amphibian_occ_data.csv", header=T, sep=",", na.strings=TRUE)
+data$Occ <- rep(1, dim(data)[1])  # Adding a column with 1s on occurrence data 
