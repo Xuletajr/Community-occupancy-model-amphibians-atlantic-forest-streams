d9553c64b68556619b27ffe77ed945f28b2458fd
Xuletajr <jwrj@rc.unesp.br>
2018-03-19 15:07:28 -0300

    Clear memory, choose folder and check filepath.

---
+++ community_model_code_amphibians.R
@@ -0,0 +1,8 @@
+##############################################################/############
+########   Amphibians of Brazilian Atlantic Rainforest streams #########
+##############   Hierarchical multi-species model   ####################
+###########################################################################
+
+rm(list=ls(all=TRUE)) # clear memory 
+choose.dir(default = "", caption = "Select folder") # choose a folder
+getwd() # Check the filepath for the working directory
